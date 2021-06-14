25e60790534e9bd007ce05d2bb0e5942f1937124
Marine <33462756+Khornya@users.noreply.github.com>
2021-06-14 21:32:25 +0200

    ✨ added consommation view in configurator (#28)

--- src/components/house-models/house-model-details/house-model-details.tsx
+++ src/components/house-models/house-model-details/house-model-details.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Button, Tab, Tabs } from "react-bootstrap";
 import { withRouter } from "react-router";
 import { HouseModelDetailsProps } from "./models";
-import Consommation from "../consommation/consommation";
+import Consommation from "../../configurator/consommation/consommation";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Estimate from "../estimate/estimate";
