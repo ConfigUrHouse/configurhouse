e835073cde76d6195ccf3a102c52114470006086
ShadowBurst <37303345+shadowburst@users.noreply.github.com>
2021-07-01 10:31:33 +0200

    Estimate in configuration details view

--- src/components/configurator/estimate/estimate.tsx
+++ src/components/configurator/estimate/estimate.tsx
@@ -1,12 +1,12 @@
 import { Component } from 'react';
 import { Col, Modal, Row, Table } from 'react-bootstrap';
-import { faEuroSign } from '@fortawesome/free-solid-svg-icons';
+import { faCalculator } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { ApiResponseError } from '../../../api/models';
 import { apiRequest } from '../../../api/utils';
 import { ConfigurationValue } from '../../../models';
 import { EstimateProps, EstimateState } from './models';
-import houseModelDetails from '../../house-models/house-model-details/house-model-details';
+import './estimate.css';
 
 class Estimate extends Component<EstimateProps, EstimateState> {
   constructor(props: EstimateProps) {
@@ -43,47 +43,47 @@ class Estimate extends Component<EstimateProps, EstimateState> {
     const { data } = this.state;
 
     return (
-      <div className="w-100 bg configuration-infos">
+      <div className="p-5 w-100 bg-white configuration-infos">
         {data && (
-          <Row>
-            <Col md={12} className="p-4">
-              <div className="rect">
-                <h2 className="text-green text-center">
-                  <FontAwesomeIcon icon={faEuroSign} /> Devis détaillé
-                </h2>
-                <Table bordered hover className="text-center mt-4">
-                  <thead>
-                    <tr>
-                      <td>Type d'option</td>
-                      <td>Option choisie</td>
-                      <td>Prix de l'option</td>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {data?.houseModel && (
-                      <tr>
-                        <td>Modèle</td>
-                        <td>{data.houseModel.name}</td>
-                        <td className="price">{data.houseModel.price}</td>
-                      </tr>
-                    )}
-                    {data?.estimate?.map((e: any, i: number) => (
-                      <tr key={i}>
-                        <td>{e.option.name}</td>
-                        <td>{e.value.name}</td>
-                        <td className="price">{e.value.price}</td>
-                      </tr>
-                    ))}
-                    <tr className="bg-lightgreen font-weight-bold">
-                      <td>Coût total des options</td>
-                      <td></td>
-                      <td className="price">{data.total}</td>
-                    </tr>
-                  </tbody>
-                </Table>
-              </div>
-            </Col>
-          </Row>
+          <div>
+            <h2 className="text-green text-center">
+              <FontAwesomeIcon icon={faCalculator} /> Devis détaillé
+            </h2>
+            <h6 className="text-center mt-2 mb-5">
+              Le devis détaillé de la configuration avec le prix du modèle et
+              des options sélectionnés.
+            </h6>
+            <Table bordered hover className="text-center my-4 mx-6">
+              <thead>
+                <tr>
+                  <td>Type d'option</td>
+                  <td>Option choisie</td>
+                  <td>Prix de l'option</td>
+                </tr>
+              </thead>
+              <tbody>
+                {data?.houseModel && (
+                  <tr>
+                    <td>Modèle</td>
+                    <td>{data.houseModel.name}</td>
+                    <td className="price">{data.houseModel.price}</td>
+                  </tr>
+                )}
+                {data?.estimate?.map((e: any, i: number) => (
+                  <tr key={i}>
+                    <td>{e.option.name}</td>
+                    <td>{e.value.name}</td>
+                    <td className="price">{e.value.price}</td>
+                  </tr>
+                ))}
+                <tr className="bg-lightgreen font-weight-bold">
+                  <td>Coût total des options</td>
+                  <td></td>
+                  <td className="price">{data.total}</td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
         )}
         {this.state.error && (
           <Modal
